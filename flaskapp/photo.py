76aa6445f006fb02a428b13621ef88caad30f843
deltaX72 <50233011+deltaX72@users.noreply.github.com>
2020-06-20 10:18:35 +0700

    Create photo.py

---
+++ flaskapp/photo.py
@@ -0,0 +1,15 @@
+import base64
+import io
+from PIL import Image, ImageEnhance
+import numpy as np
+
+def resize_image(image, coefficient):
+    h, w = image.size
+    resized_image = image.resize((int(h * coefficient), int(w * coefficient)))
+    return resized_image
+
+def convert_to_rgb(base64_string, index):
+    data = base64.b64decode(str(base64_string))
+    filename = f'static/photo{index}.jpg' 
+    with open(filename, 'wb') as f:
+        f.write(data)
